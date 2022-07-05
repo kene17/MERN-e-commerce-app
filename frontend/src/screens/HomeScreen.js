40410248a3970b186925d8a31ef3ba48cc5d9e8e
kojukwu <keneojukwu17@gmail.com>
2022-07-05 00:05:10 -0400

    media queries for homescreen and product

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import Product from '../components/Product'
+import './HomeScreen.css'
 const HomeScreen = () => {
     return (
-        <div>
-            home
+        <div className='homescreen'>
+            <h2 className="homescreen__title">
+                <div className="homescreen__products">
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    
+                </div>
+            </h2>
         </div>
     )
 }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const ProductScreen = () => {
-    return (
-        <div>
-            product
-        </div>
-    )
-}
+  return (
+    <div className="productscreen">
+      <div className="productscreen__left"></div>
+      <div className="productscreen__right"></div>
+    </div>
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
